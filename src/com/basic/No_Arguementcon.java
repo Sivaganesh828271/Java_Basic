c06fb848ab234428b9afe404c779300ca2d7f763
Sivaganesh828271 <siva828271@gmail.com>
2023-06-05 13:48:42 +0530

    Java basic

---
+++ src/com/basic/No_Arguementcon.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class No_Arguementcon {
+	//non parameterized constructor
+	public No_Arguementcon() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Welcome to nyinst");
+	}
+	public static void main(String[] args) {
+		No_Arguementcon a = new No_Arguementcon();
+	
+		
+	}
+
+}
---
+++ src/com/basic/Parameterizedcon.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class Parameterizedcon {
+	public Parameterizedcon(int a) {
+		// TODO Auto-generated constructor stub
+		System.out.println("The value of a is:"+a);
+	}
+	public static void main(String[] args) {
+		Parameterizedcon a = new Parameterizedcon(23);
+	}
+
+}
